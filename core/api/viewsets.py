87abda46a7b8fc127637350606da90dd0fe8c619
Alessandre Martins <sandremartins@gmail.com>
2021-03-23 12:00:24 -0400

    Implementando imagens na API

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -11,3 +13,29 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    def list(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
+
+    @action(methods=['post', 'get'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+
+    @action(methods=['get'], detail=False)
+    def teste(self, request):
+        pass
